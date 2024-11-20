ae68ac4047a1e09148d207d871a4d82bd7d159c2
sary <saryr.r22@gmail.com>
2024-11-20 20:14:10 +0700

    add file

--- app/src/main/java/com/example/numthesis/MainActivity.kt
+++ app/src/main/java/com/example/numthesis/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.numthesis
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+//        btnGetin
+        val btnGetin = findViewById<Button>(R.id.getinbtn)
+        btnGetin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
