19fe66cc40782a4850c49f775f9c85e3f4f3c15d
sary <saryr.r22@gmail.com>
2024-11-25 18:53:31 +0700

    add file

--- app/src/main/java/com/example/numthesis/AccountFragment.kt
+++ app/src/main/java/com/example/numthesis/AccountFragment.kt
@@ -1,18 +1,33 @@
 package com.example.numthesis
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.ActivityChooserView.InnerLayout
 
+@Suppress("UNREACHABLE_CODE")
 class AccountFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_account, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+
+        //return inflater.inflate(R.layout.fragment_account, container, false)
+        val btnLogout = view.findViewById<Button>(R.id.btnLogout)
+
+        btnLogout?.setOnClickListener {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+        return view
     }
 }
