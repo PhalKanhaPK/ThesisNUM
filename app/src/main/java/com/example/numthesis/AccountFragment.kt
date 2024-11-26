372bc6d512c259893a9741ed320bbb5dfed4826c
sary <saryr.r22@gmail.com>
2024-11-26 18:48:45 +0700

    edit file

--- app/src/main/java/com/example/numthesis/AccountFragment.kt
+++ app/src/main/java/com/example/numthesis/AccountFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.widget.ActivityChooserView.InnerLayout
 
 class AccountFragment : Fragment() {
@@ -16,16 +18,19 @@ class AccountFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_account, container, false)
-
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_account, container, false)
         val btnLogout = view.findViewById<Button>(R.id.btnLogout)
-
         btnLogout?.setOnClickListener {
             val intent = Intent(requireContext(), LoginActivity::class.java)
             startActivity(intent)
-
         }
+
+        val arrayAdapter: ArrayAdapter<*>
+        val userInfo = arrayOf("ជំនាញ : IT","ជំនាន់ : ៣០","ក្រុម : ៤៥","លេខទូរស័ : 0987654321","Gmail : example@gmail.com")
+
+        var mListView = view.findViewById<ListView>(R.id.ListView_userInfo)
+        arrayAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1, userInfo)
+        mListView.adapter = arrayAdapter
+
         return view
     }
 }
