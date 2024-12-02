d9bb19a6377ebef1e984a3c25d0c28c56c411676
sary <saryr.r22@gmail.com>
2024-12-02 20:20:43 +0700

    edit file

--- app/src/main/java/com/example/numthesis/GroupFragment.kt
+++ app/src/main/java/com/example/numthesis/GroupFragment.kt
@@ -1,19 +1,16 @@
 package com.example.numthesis
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-class FavoriteFragment : Fragment() {
+class GroupFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        return inflater.inflate(R.layout.fragment_group, container, false)
     }
-
-
 }
--- app/src/main/java/com/example/numthesis/HomeFragment.kt
+++ app/src/main/java/com/example/numthesis/HomeFragment.kt
@@ -17,7 +17,13 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val cardView = view.findViewById<CardView>(R.id.card1)
         cardView?.setOnClickListener{
-
+            // Create an instance of the fragment you want to display
+            val groupFragment = GroupFragment()
+           // Replace the current fragment with the new fragment
+           val transaction = parentFragmentManager.beginTransaction()
+           transaction.replace(R.id.fragment_home, groupFragment) // Use the ID of your container
+           transaction.addToBackStack(null) // Optional: Add to back stack to allow user to navigate back
+           transaction.commit()
         }
 
         return view
