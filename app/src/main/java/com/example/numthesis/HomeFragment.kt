1efce564c946801872aad4bb29eefd47273d9353
sary <saryr.r22@gmail.com>
2024-12-04 14:39:07 +0700

    edit file

--- app/src/main/java/com/example/numthesis/HomeFragment.kt
+++ app/src/main/java/com/example/numthesis/HomeFragment.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 
+data class Generation(
+    val genName: String = "" // Holds the generation name in Khmer
+)
+
 class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,17 +18,14 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-        val cardView = view.findViewById<CardView>(R.id.card1)
-        cardView?.setOnClickListener{
-            val groupFragment = ThesisFragment()
-           val transaction = parentFragmentManager.beginTransaction()
-           transaction.replace(R.id.fragmentContainerView, groupFragment)
-           transaction.addToBackStack(null)
-           transaction.commit()
-
-//            val intent = Intent(requireContext(), GroupFragment::class.java)
-//            startActivity(intent)
-        }
+//        val cardView = view.findViewById<CardView>(R.id.card1)
+//        cardView?.setOnClickListener{
+//            val groupFragment = ThesisFragment()
+//           val transaction = parentFragmentManager.beginTransaction()
+//           transaction.replace(R.id.fragmentContainerView, groupFragment)
+//           transaction.addToBackStack(null)
+//           transaction.commit()
+//        }
 
         return view
     }
